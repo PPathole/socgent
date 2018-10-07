94f1037b206a86f5d29e31f609e75d864399e299
Rajan Pandey <rajanpandey2508@gmail.com>
2018-10-07 23:19:42 +0530

    Fixed all the errors

--- insert.php
+++ insert.php
@@ -1,5 +1,5 @@
 <?php    
-    $con = mysqli_connect('fdb23.your-hosting.net', '2846452_society', 'Society123', '2846452_society');
+    require('connect.php');
 
     if(mysqli_connect_error()){  
         die('Connect Error('.mysqli_connect_errno().')'.mysqli_connect_error());  
@@ -12,7 +12,7 @@
         $flatno=mysqli_real_escape_string($con,$_POST['flat']);
         $phoneno=mysqli_real_escape_string($con,$_POST['phone']);
 
-        $sql = "INSERT INTO users (name,email,password,age,bldgname,flatno,phoneno) 
+        $sql = "INSERT INTO players (name,email,password,age,bldgname,flatno,phoneno) 
             VALUES('$name','$email','$password','$age','$building','$flatno','$phoneno')";
 
         if(mysqli_query($con, $sql)){
@@ -21,7 +21,7 @@
             window.location='registrationpage.html';
             </script>";      
         }else {
-            echo "Error".$sql."<br>".$conn->error;
+            echo "Error".$sql."<br>".$con->error;
         }  
     }
 ?>
